5eb832ce17b85cff62a93757a8e3087cbee95280
Niels Bugel <bugel.niels@gmail.com>
2023-06-17 15:08:17 +0200

    Added build scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Cleans up build files
+
 cd ..
 rm build -rf
-rm *.AppImage
 cd appdir || exit
 rm AppRun
 rm *.desktop
 cd usr || exit
 rm bin/ -rf
 rm lib/ -rf
+rm share/doc -rf
 rm plugins/ -rf
 rm translations/ -rf
 cd ..
